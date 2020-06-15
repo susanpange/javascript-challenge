0dcbaad7b39b093367306698dca2772527e6f2e6
susanpange <60121676+susanpange@users.noreply.github.com>
2020-06-14 22:38:19 -0400

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,14 +3,24 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("#ufo-table");
-var input_form = d3.select("#datetime");
-//var date_search_button = d3.select("#filter-btn");
+var input_date = d3.select("#datetime");
+var input_city = d3.select("#city");
+var input_state = d3.select("#state");
+var input_country = d3.select("#country");
+var input_shape = d3.select("#shape");
+var date_search_button = d3.select("#filter-btn");
 
-function datetime_search() {
+function search() {
 
-    var input_date = d3.event.target.value;
+    var input_date_value = input_date.property("value");
+    var input_city_value = input_city.property("value");
+    var input_state_value = input_state.property("value");
+    var input_country_value = input_country.property("value");
+    var input_shape_value = input_shape.property("value");
     tbody.selectAll("td").remove();
-    var search_result = tableData.filter(data_entry => data_entry.datetime == input_date);
+    //var search_result = tableData.filter(data_entry => data_entry.datetime == input_date_value && data_entry.city == input_city_value && data_entry.state == input_state_value && data_entry.country == input_country_value && data_entry.shape == input_shape_value);
+    //var search_result = tableData.filter(data_entry => data_entry.datetime == input_date_value && data_entry.city == input_city_value && data_entry.state == input_state_value && data_entry.country == input_country_value);
+    var search_result = tableData.filter(data_entry => data_entry.shape == input_shape_value);
     search_result.forEach((data_entry) => {
         var row = tbody.append("tr");
         Object.values(data_entry).forEach((data_element) =>{
@@ -20,5 +30,5 @@ function datetime_search() {
     console.log(search_result);
 }
 
-input_form.on("change", datetime_search);
-//date_search_button.on("click", datetime_search);
+//d3.selectAll("li").on("change", search);
+date_search_button.on("click", search);
